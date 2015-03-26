8fbfd54c75a2d53b08004f6e9524d8181da7c26d
Tony Cheng <shilmista@gmail.com>
2015-03-26 12:47:03 -0400

    Initial Commit

---
+++ Strap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Strap
+//
+//  Created by Tony Cheng on 3/26/15.
+//  Copyright (c) 2015 Tony Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Strap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Strap
+//
+//  Created by Tony Cheng on 3/26/15.
+//  Copyright (c) 2015 Tony Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
